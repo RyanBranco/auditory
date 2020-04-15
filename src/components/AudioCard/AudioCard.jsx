ec99272848ccedf6ddf1bdfcad75067bc35fe57b
Ryan Branco <ryanbr23@gmail.com>
2020-04-15 11:58:37 -0700

    audio card style

--- src/components/AudioCard/AudioCard.jsx
+++ src/components/AudioCard/AudioCard.jsx
@@ -13,15 +13,22 @@ class AudioCard extends Component {
     render() {
         return (
             <div id={styles.AudioCard}>
+                <i id={styles.options} className="material-icons">more_vert</i>
                 <img src={placeholder} alt="placeholder"></img>
-                <p id={styles.title}>This is where the title will go</p>
-                <audio controls>
-                    <source src="horse.mp3" type="audio/mpeg"></source>
-                </audio>
-                <p id={styles.username}>by (username will go here)</p>
-                <i className="material-icons">thumb_up</i>
-                <i className="material-icons">thumb_down</i>
-                <i className="material-icons">chat</i>
+                <div id={styles.cardContents}>
+                    <p id={styles.title}>This is where the title will go</p>
+                    <p id={styles.username}>by (username will go here)</p>
+                    <div id={styles.icons}>
+                        <i id={styles.AudioCardControls} className="material-icons">thumb_up</i>
+                        <i id={styles.AudioCardControls} className="material-icons">thumb_down</i>
+                        <i id={styles.AudioCardControls} className="material-icons">chat</i>
+                    </div>
+                </div>
+                <div id={styles.controller}>
+                    <audio controls>
+                        <source src="horse.mp3" type="audio/mpeg"></source>
+                    </audio>
+                </div>
             </div>
         )
     }
